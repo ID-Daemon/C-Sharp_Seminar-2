e6f04adbc7f8ad20934cd0d04383bd7cd74161f2
Neuf eGO <patronus1994@gmail.com>
2022-10-07 13:02:26 +0500

    View only seminar work

--- Program.cs
+++ Program.cs
@@ -69,50 +69,50 @@ if ((number2 * number2) != number1 || (number1 * number1) != number2) Console.Wr
 else Console.WriteLine("Числа не являются квадратом друг-друга");
 
 
-//Переменные для ДЗ
-// int number;
+// //Переменные для ДЗ
+// // int number;
 
-// Домашнее задание
+// // Домашнее задание
 
-Console.WriteLine();Console.WriteLine();
-Console.WriteLine("Задача №10.");
-Console.WriteLine();
+// Console.WriteLine();Console.WriteLine();
+// Console.WriteLine("Задача №10.");
+// Console.WriteLine();
 
-Console.Write("Введите трехзначное число: ");
-number = int.Parse(Console.ReadLine()!);
-while (number < 100 || number > 999){
-    Console.Write("Вы ввели НЕ трехзначное число. Пожалуйста, введите корректное число: ");
-    number = int.Parse(Console.ReadLine()!); 
-}
+// Console.Write("Введите трехзначное число: ");
+// number = int.Parse(Console.ReadLine()!);
+// while (number < 100 || number > 999){
+//     Console.Write("Вы ввели НЕ трехзначное число. Пожалуйста, введите корректное число: ");
+//     number = int.Parse(Console.ReadLine()!); 
+// }
 
-number2 = (number / 10) % 10;
-Console.WriteLine($"Втрорая цифра введенного числа {number} - {number2}");
+// number2 = (number / 10) % 10;
+// Console.WriteLine($"Втрорая цифра введенного числа {number} - {number2}");
 
-Console.WriteLine();Console.WriteLine();
-Console.WriteLine("Задача №13.");
-Console.WriteLine();
+// Console.WriteLine();Console.WriteLine();
+// Console.WriteLine("Задача №13.");
+// Console.WriteLine();
 
-// По идее я написал обработку числа до трехзначного, поэтому ограничение числа до 100.000 не актуально.
-// do {
-    Console.Write("Введите число: ");
-    number = int.Parse(Console.ReadLine()!);
-// } while (number > 100000);
+// // По идее я написал обработку числа до трехзначного, поэтому ограничение числа до 100.000 не актуально.
+// // do {
+//     Console.Write("Введите число: ");
+//     number = int.Parse(Console.ReadLine()!);
+// // } while (number > 100000);
 
-while (number > 999) {
-    if (number < 100) break;
-    number = number / 10;
-} 
+// while (number > 999) {
+//     if (number < 100) break;
+//     number = number / 10;
+// } 
 
-if (number < 100) Console.WriteLine("Третьей фифры нет");
-else Console.WriteLine("->" + number % 10);
+// if (number < 100) Console.WriteLine("Третьей фифры нет");
+// else Console.WriteLine("->" + number % 10);
 
-Console.WriteLine();Console.WriteLine();
-Console.WriteLine("Задача №15.");
-Console.WriteLine();
+// Console.WriteLine();Console.WriteLine();
+// Console.WriteLine("Задача №15.");
+// Console.WriteLine();
 
-Console.Write("Введите номер дня недели: ");
-number = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите номер дня недели: ");
+// number = int.Parse(Console.ReadLine()!);
 
-if (number < 1 || 7 < number) Console.WriteLine("Нет такого номера недели");
-else if (number == 6 || number == 7) Console.WriteLine("Вы ввели номер выходного дня недели");
-else Console.WriteLine("Это не выходной");
+// if (number < 1 || 7 < number) Console.WriteLine("Нет такого номера недели");
+// else if (number == 6 || number == 7) Console.WriteLine("Вы ввели номер выходного дня недели");
+// else Console.WriteLine("Это не выходной");
